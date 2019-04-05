f0bfe4a901089248e393fe8e9d5451f1a1c77ed3
Code Hz <CodeHz@outlook.com>
2019-04-05 21:18:56 +0800

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -ex
+
+mkdir -p build dist
+base_path=$(pwd)
+cd build
+eval $* cmake -DCMAKE_INSTALL_PREFIX:PATH="$base_path/dist" -DCMAKE_BUILD_TYPE=MinSizeRel ..
+make
+make install
