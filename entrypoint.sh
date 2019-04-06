19b57b3c69a7a91c84acf70fd090a2d930268cc3
Code Hz <CodeHz@outlook.com>
2019-04-06 22:27:08 +0800

    fix git submodule

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -ex
 
 mkdir -p build dist
 base_path=$(pwd)
+git submodule update --init --recursive
 cd build
 eval $* cmake -DCMAKE_INSTALL_PREFIX:PATH="$base_path/dist" -DCMAKE_BUILD_TYPE=MinSizeRel ..
 make
